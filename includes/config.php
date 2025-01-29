f7ec5e82e4a9e95b572c82c882ef49e53bf32774
Amirddx <103104559+Amirddx@users.noreply.github.com>
2025-01-29 20:04:51 +0300

    Partial implementation of Google API

---
+++ includes/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('GOOGLE_SPREADSHEET_ID', '1M6WJwYjPCAPJhpovpbS-szkbAmBMNeR0AaOdE1eNAyE'); // ID google Таблицы
+define('GOOGLE_KEY_PATH', __DIR__ . '/../config/google/google-key.json'); // путь к API ключу
+
--- jobs.php
+++ jobs.php
@@ -1,23 +1,43 @@
+<?php
+include __DIR__ . '/includes/navbar.php';
+require __DIR__ . '/services/google-sheets.php';
+
+$range = 'Vacancies!A2:I'; // брать только Город (A) и Адрес (I)
+$data = getGoogleSheetData($range);
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Вакансии</title>
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/css/metiztorg-style.css">
+    <title>Открытые Вакансии</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/metiztorg-style.css">
 </head>
-<body class="d-flex flex-column min-vh-100">
-
-<?php include __DIR__ .'/includes/navbar.php'; ?>
+<body>
 
 
-<main class="container my-5 flex-grow-1">
+<main class="container my-5">
+    <h3 class="text-center mb-4">Открытые Вакансии</h3>
 
-
+    <?php if (!empty($data)): ?>
+        <ul class="list-group">
+            <?php foreach ($data as $row): ?>
+                <li class="list-group-item">
+                     <strong><?php echo htmlspecialchars($row[0] ?? '—'); ?></strong>,
+                    <?php echo htmlspecialchars($row[8] ?? '—'); ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
+        <p class="text-center text-muted">Вакансий пока нет.</p>
+    <?php endif; ?>
 </main>
 
-<?php include __DIR__.'/includes/maps.php';?>
+<?php //include __DIR__ . '/includes/footer.php'; ?>
 
 </body>
 </html>
